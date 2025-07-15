17c7ad00a96b9598dc17aab527069939861df991
Powerm1nt <leonard.chabot@nuka.works>
2025-07-15 07:54:16 +0200

    Impl Tooltip

---
+++ Web/Common/BoundingRect.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Common
+{
+    public class BoundingRect
+    {
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public double Bottom { get; set; }
+        public double Right { get; set; }
+    }
+}
---
+++ Web/wwwroot/js/utils.js
@@ -0,0 +1,33 @@
+﻿window.getBoundingClientRect = (element) => {
+    if (!element || typeof element.getBoundingClientRect !== 'function') {
+        console.error('Invalid element passed to getBoundingClientRect');
+        return null;
+    }
+
+    const rect = element.getBoundingClientRect();
+
+    return {
+        top: Math.round(rect.top),
+        left: Math.round(rect.left),
+        bottom: Math.round(rect.bottom),
+        right: Math.round(rect.right),
+        width: Math.round(rect.width),
+        height: Math.round(rect.height)
+    };
+};
+
+(function () {
+    let resizeHandler = null;
+
+    window.registerResizeHandler = (dotNetRef) => {
+        if (resizeHandler) {
+            window.removeEventListener("resize", resizeHandler);
+        }
+
+        resizeHandler = () => {
+            dotNetRef.invokeMethodAsync("UpdateTooltipPosition");
+        };
+
+        window.addEventListener("resize", resizeHandler);
+    };
+})();
